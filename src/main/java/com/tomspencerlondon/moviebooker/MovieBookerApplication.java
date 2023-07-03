fe0d7112523cc8351a8df16832148169fd0c2e17
Dmitri Bontoft <dmitri.bontoft@gmail.com>
2023-07-03 08:59:54 +0100

    Change bootstrap to use admin functions for creating movies and programs

--- src/main/java/com/tomspencerlondon/moviebooker/MovieBookerApplication.java
+++ src/main/java/com/tomspencerlondon/moviebooker/MovieBookerApplication.java
@@ -1,8 +1,10 @@
 package com.tomspencerlondon.moviebooker;
 
-import com.tomspencerlondon.moviebooker.moviegoer.hexagon.application.MovieService;
-import com.tomspencerlondon.moviebooker.moviegoer.hexagon.domain.Movie;
-import com.tomspencerlondon.moviebooker.moviegoer.hexagon.domain.MovieProgram;
+import com.tomspencerlondon.moviebooker.admin.hexagon.application.AdminMovieService;
+import com.tomspencerlondon.moviebooker.admin.hexagon.application.AdminProgramService;
+import com.tomspencerlondon.moviebooker.admin.hexagon.domain.AdminMovie;
+import com.tomspencerlondon.moviebooker.admin.hexagon.domain.AdminProgram;
+import com.tomspencerlondon.moviebooker.admin.hexagon.domain.Screen;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,8 +19,10 @@ import java.time.LocalDateTime;
 public class MovieBookerApplication implements CommandLineRunner {
 
     @Autowired
-    MovieService movieService;
+    AdminMovieService movieService;
 
+    @Autowired
+    AdminProgramService adminProgramService;
     public static void main(String[] args) {
         SpringApplication.run(MovieBookerApplication.class, args);
     }
@@ -30,19 +34,20 @@ public class MovieBookerApplication implements CommandLineRunner {
 
     private void bootstrapData() {
         String description = "Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando). When the don's youngest son, Michael (Al Pacino), reluctantly joins the Mafia, he becomes involved in the inevitable cycle of violence and betrayal. Although Michael tries to maintain a normal relationship with his wife, Kay (Diane Keaton), he is drawn deeper into the family business.";
-        Movie movie = new Movie(1L,
+        AdminMovie movie = new AdminMovie(1L,
                 "Godfather",
                 "/img/godfather.jpg",
                 LocalDate.of(1975, 8, 24)
                 , description);
 
+        Screen screen = new Screen(null,20);
 
-        Movie savedMovie = movieService.saveMovie(movie);
-        MovieProgram movieProgram = new MovieProgram(1L,
+        AdminMovie savedMovie = movieService.save(movie);
+        var movieProgram = new AdminProgram(1L,
                 LocalDateTime.of(2023, 2, 14, 13, 0),
-                20,
+                screen,
                 savedMovie, 0, new BigDecimal("5.00"));
 
-        movieService.saveMovieProgram(movieProgram);
+        adminProgramService.save(movieProgram);
     }
 }
